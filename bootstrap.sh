d6d187d5bb2bc6a325b233ac887a7b45967223c7
James Lance <jalance@Sting.local>
2014-07-08 13:06:58 -0600

    reload vim docs after updating

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,8 @@ function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "init" -avh --no-perms . ~
 	source ~/.bash_profile
+    #reload the vim doc files
+    vim -c "helptags ~/.vim/doc" -c q
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
