336b26154ae5f1de7f3f9b774bfa221cee30fb9a
James Lance <jalance@bluehost.com>
2016-10-27 10:02:05 -0600

    changed vim-plug command to plugupdate

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ function doIt() {
         echo "sourcing bash"
         source ~/.bash_profile
     fi
-    vim +PlugInstall +qall
+    vim +PlugUpdate +qall
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
