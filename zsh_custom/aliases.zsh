848551abf55d282e71b784575b1a685f432b89af
James Lance <jalance@Anduril.home.thelances.net>
2021-08-20 10:42:33 -0600

    added a print out for dig so I can remember the dccacheutil command Updated my `git lol` alias

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -9,3 +9,6 @@ alias brewski='brew update && brew upgrade && brew cu && brew cleanup; brew doct
 
 # cleanup unused docker images, layers, etc
 alias docker-cleanup='docker run --rm -v /var/run/docker.sock:/var/run/docker.sock:rw -v /var/lib/docker:/var/lib/docker:rw -e LOOP=false -e DELAY_TIME=10 meltwater/docker-cleanup:latest'
+
+# remind me what the Mac Os command for dig is again?
+function dig() { if [[ `uname` == "Darwin" ]]; then echo "try dscacheutil -q host -a name $1"; fi; command dig $@ }
