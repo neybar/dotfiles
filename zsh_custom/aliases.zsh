02de89a878683455601975f8ca0090532a071c33
James Lance <james@thelances.net>
2022-10-26 19:11:37 -0600

    added new iterm profile plus a few other things

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -10,5 +10,8 @@ alias brewski='brew update && brew upgrade && brew cu && brew cleanup; brew doct
 # cleanup unused docker images, layers, etc
 alias docker-cleanup='docker run --rm -v /var/run/docker.sock:/var/run/docker.sock:rw -v /var/lib/docker:/var/lib/docker:rw -e LOOP=false -e DELAY_TIME=10 meltwater/docker-cleanup:latest'
 
+# allow wildcard in scp command: https://superuser.com/questions/420525/scp-with-zsh-no-matches-found
+alias scp='noglob scp'
+
 # remind me what the Mac Os command for dig is again?
 function dig() { if [[ `uname` == "Darwin" ]]; then echo "try dscacheutil -q host -a name $1"; fi; command dig $@ }
